66d7ddb1981771ab233182a38acfbf6213acafb1
Loznek <kenzio4444@gmail.com>
2024-11-25 16:11:41 +0100

    Make Creation DTO-os suitable for frontend

--- TransportAdministrationBE/src/main/kotlin/com/transportmanagement/DTOs/DriverCreation.kt
+++ TransportAdministrationBE/src/main/kotlin/com/transportmanagement/DTOs/DriverCreation.kt
@@ -1,7 +1,9 @@
 package com.transportmanagement.DTOs
 
 import kotlinx.datetime.LocalDateTime
+import kotlinx.serialization.Serializable
 
+@Serializable
 class DriverCreation(
     val name: String,
     val dateOfBirth: LocalDateTime,
